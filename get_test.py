7465ff17b4d791190d75b3a518ec6c90d84c16f2
Jacob Harris <jacob.harris.122@gmail.com>
2018-07-21 11:34:22 -0400

    get links from wiki page

--- get_test.py
+++ get_test.py
@@ -1,11 +1,29 @@
 from requests import get
+from bs4 import BeautifulSoup
 
 url = 'https://en.wikipedia.org/wiki/Computer_security'
 
 response = get(url)
 #print(response.text[:500])
 
-with open ("test.html", 'wb') as f_out:
+with open ("outputs/test.html", 'wb') as f_out:
     f_out.write(response.text.encode('utf-8'))
 
-print("done")
+print("saved off html")
+
+html_soup = BeautifulSoup(response.text, 'html.parser')
+print(type(html_soup))
+
+text = html_soup.get_text();
+print("===================================================================")
+print("text:")
+print(text)
+print("===================================================================")
+
+print("===================================================================")
+print("links:")
+links = html_soup.find_all('a')
+for link in links:
+    print(link.get('href'))
+print(f'number of links: {len(links)}')
+print("===================================================================")
